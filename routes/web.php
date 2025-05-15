cdda75fdc109092375d48d99261e03bd30f25b9b
febriekaa <febryekhawahyuni95@gmail.com>
2025-05-15 23:36:49 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/services', [ServiceController::class, 'index']);
+Route::post('/bookings', [BookingController::class, 'store']);
